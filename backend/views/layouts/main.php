d1ff9e25cee040cde4c8c6c55ca0211bc05336d0
Black Ghost <mcramiel@gmail.com>
2021-05-01 19:20:28 +0300

    update main

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -103,7 +103,7 @@ AppAsset::register($this);
  
     <div class="container">
         <?= Breadcrumbs::widget(['homeLink' => [
-            'label' => 'Головна'
+            'label' => 'Main',
         ],
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
