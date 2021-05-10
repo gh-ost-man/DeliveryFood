79eb69bd1c184ca1d6865f473a2a237d3e4c2c92
Black Ghost <mcramiel@gmail.com>
2021-05-10 16:49:51 +0300

    create ShopController

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -4,8 +4,8 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use common\widgets\Alert;
@@ -19,6 +19,9 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
@@ -26,45 +29,75 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap">
+<div class="wrap text-white">
     <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
+        $this->registerCss(
+        " 
+            #w0-collapse
+            {
+                flex-grow: 0; 
+            }
+            #menu 
+            {
+                list-type: none;
+            }
+            .bg-nav
+            {
+                background-color: rgba(34,38,42,255);
+            }
+        "
+        );
+
+        NavBar::begin([
+            'brandLabel' => Yii::$app->name,
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar navbar-dark bg-nav  navbar-expand-md ',
+            ],
+        ]);
+        // $menuItems = [
+        //     ['label' => 'Home', 'url' => ['/site/index']],
+        //     ['label' => 'About', 'url' => ['/site/about']],
+        //     ['label' => 'Contact', 'url' => ['/site/contact']],
+        // ];
+        if (Yii::$app->user->isGuest) {
+            $menuItems[] = ['label' => 'About us', 'url' => ['#']];
+            $menuItems[] = ['label' => 'Basket', 'url' => ['#']];
+            $menuItems[] = ['label' => 'Sign up', 'url' => ['#']];
+            $menuItems[] = ['label' => 'Login', 'url' => ['#']];
+        } else {
+            $menuItems[] = '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn text-white']
+                )
+                . Html::endForm()
+                . '</li>';
+        }
+
+       
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav nav-dark nav-pills'],
+            'items' => $menuItems,
+        ]);
+
+    
+        if(Yii::$app->user->id != null){
+            echo Html::tag('a', 'Basket', ['class' => 'btn btn-info', 'href' => Yii::$app->homeUrl . 'tovar/basket']);
+        }
+        ?>
+        <?php
+    
+        NavBar::end();
+      
     ?>
 
-    <div class="container">
+    <div class="container ">
         <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+          'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
-        <?= Alert::widget() ?>
+        <!-- <?= Alert::widget() ?> -->
         <?= $content ?>
     </div>
 </div>
