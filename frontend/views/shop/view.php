7317fc4f54cd64450196b052fc9a00dddd6dee83
Black Ghost <mcramiel@gmail.com>
2021-05-15 11:54:03 +0300

    update

--- frontend/views/shop/view.php
+++ frontend/views/shop/view.php
@@ -22,8 +22,8 @@
         <div class="card h-100">
             <img src="/<?= $image[0]?>" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title"><?= $value->title?></h5>
-              <p class="card-text"><?= $value->description?></p>  
+              <h4 class="card-title text-center" ><?= $value->title?></h4>
+              <p class="card-text" style="text-align: justify;"><?= $value->description?></p>  
             </div>
             <div class="card-footer">
                 <h4 class="float-left w-50"><?= $value->price?>$</h4>
